2894d2441955b7e42dbd9d9c5fba54d2f051a389
Leandro Serra <leandro.serra.10@gmail.com>
2018-05-26 18:50:24 -0300

    First Commit

--- gestao_produtos/urls.py
+++ gestao_produtos/urls.py
@@ -6,9 +6,13 @@ from django.contrib.auth import views as auth_views
 from produtos import urls as produtos_urls
 from home import urls as home_urls
 
+
 urlpatterns = [
     path('', include(home_urls)),
     path('login/', auth_views.login, name='login'),
     path('produtos/', include(produtos_urls)),
     path('admin/', admin.site.urls),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
